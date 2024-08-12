ec741d4cb80f752010598b2168659adc7e1c6c0c
jeongdong-gi <wjdehdrl7485@gmail.com>
2024-08-12 16:08:39 +0900

    update

--- oopPractice/src/main/java/org/example/Calculator.java
+++ oopPractice/src/main/java/org/example/Calculator.java
@@ -1,14 +1,18 @@
 package org.example;
 
-public class Calculator {
+import org.example.calculate.*;
 
-    public static int calculate(int operand1, String operator, int operand2) {
-        if ("+".equals(operator)) {
-            return operand1 + operand2;
-        }
-        else if("-".equals(operator)) {
-            return operand1 - operand2;
-        }
-        return 0;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Calculator {
+    private static final List<NewArithmeticOperator> arithmeticOperators = List.of(new AdditionOperator(), new SubtractionOperator(), new MultiplicationOperator(), new DivisionOperator());
+
+    public static int calculate(PositiveNumber operand1, String operator, PositiveNumber operand2) {
+        return arithmeticOperators.stream()
+                .filter(arithmeticOperator -> arithmeticOperator.supports(operator))
+                .map(arithmeticOperator ->arithmeticOperator.calculate(operand1, operand2))
+                .findFirst()
+                .orElseThrow(()-> new IllegalArgumentException(("올바른 사칙 연산이 아닙니다.")));
     }
 }
--- oopPractice/src/test/java/org/example/CalculatorTest.java
+++ oopPractice/src/test/java/org/example/CalculatorTest.java
@@ -1,9 +1,17 @@
 package org.example;
 
+import org.example.calculate.PositiveNumber;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 /*
     요구사항
@@ -16,20 +24,30 @@ public class CalculatorTest {
 
     //  1 + 2 ----> Calculator
     @DisplayName("덧셈 연산을 수행한다.")
-    @Test
-    void additionTest() {
+    @ParameterizedTest
+    @MethodSource("formulaAndResult")
+    void calculateTest(int operand1, String operator, int operand2, int result) {
 
-        int result = Calculator.calculate(1, "+", 2);
+        int calculateResult = Calculator.calculate(new PositiveNumber(operand1), operator, new PositiveNumber(operand2));
 
-        assertThat(result).isEqualTo(3);
+        assertThat(calculateResult).isEqualTo(result);
     }
 
-    @DisplayName("뺄셈 연산을 수행한다.")
-    @Test
-    void subtractionTest() {
-
-        int result = Calculator.calculate(3, "-", 2);
-
-        assertThat(result).isEqualTo(1);
+    private static Stream<Arguments> formulaAndResult() {
+        return Stream.of(
+                arguments(1, "+", 2, 3),
+                arguments(1, "-", 2, -1),
+                arguments(4, "*", 2, 8),
+                arguments(4, "/", 2, 2)
+        );
     }
+
+    @DisplayName("나눗셈에서 0을 나누는 경우 IllegalArgument 예외를 발생시킨다.")
+    @Test
+    void calculateExceptionTest() {
+        assertThatCode(()->Calculator.calculate(new PositiveNumber(10), "/", new PositiveNumber(0)))
+            .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("0으로 나눌 수 없습니다.");
+    }
+
 }
